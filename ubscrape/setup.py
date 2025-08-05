643633c2fa4c1f756137c10da7c0013deb6d96cd
TJ Heid <169084002+ThatsRight-ItsTJ@users.noreply.github.com>
2025-08-05 18:34:30 -0500

    Fix bs4 dependency issues

--- ubscrape/setup.py
+++ ubscrape/setup.py
@@ -13,7 +13,7 @@ setup(
         'console_scripts': ['ubscrape = ubscrape.command_line:main']
     },
     install_requires=[
-        'bs4',
+        'beautifulsoup4',
         'requests'
     ],
 )
